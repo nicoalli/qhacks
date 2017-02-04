5ec466e7679e57f7ea9a26455a73e61b08d7da38
jennyzhang7 <jennyy.zhang@mail.utoronto.ca>
2017-02-04 14:18:25 -0500

    added more imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash redirect, session, abort
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
