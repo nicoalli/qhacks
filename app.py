123e7ea9837064ad93716d0b5e90d32ac8c38828
jennyzhang7 <jennyy.zhang@mail.utoronto.ca>
2017-02-04 14:17:15 -0500

    added new routes for login

--- app.py
+++ app.py
@@ -68,8 +68,25 @@ def signUp():
         return "Error"
     else:
         return "Success"
+    
+    
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+                               
+@app.route('/login', methods=['POST'])
+def do_admin_login():          
+    if request.form['password'] == 'password' and request.form['username'] == 'qhacks':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
 
 
  
 if __name__ == "__main__":
-    app.run()
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
