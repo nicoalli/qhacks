13ca77b7ca487ddbae8961ba90b85550468cc4a4
Nicole Alli <alli.nicolette@gmail.com>
2017-02-04 08:36:59 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'allini'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'icilaWb0!'
+app.config['MYSQL_DATABASE_DB'] = 'qhacks'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route("/")
+def main():
+    return render_template('signup.html')
+
+@app.route("/index")
+def loogedIn():
+    _name = request.args.get('name')
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name FROM users WHERE name='" + _name + "'")
+    name = cursor.fetchone()
+
+    cursor.execute("SELECT question FROM users WHERE name='" + _name + "'")
+    question = cursor.fetchone()
+    
+    return render_template('index.html', name=name, question=question)
+
+@app.route("/question", methods=['POST'])
+def dispay_question():
+    _category = request.form['category']
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT question FROM questions WHERE category='" + _category + "'")
+    data = cursor.fetchall()
+
+    return render_template('question.html', question=data)
+
+@app.route("/save", methods=['POST'])
+def save_settings():
+    _userQ = request.form['q']
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO users (question) VALUES (" + _userQ + ")")
+    data = cursor.fetchall()
+
+    if len(data) is 0:
+        return "Error"
+    else:
+        return "Success"
+           
+@app.route("/signup", methods=['POST'])
+def signUp():
+    _name = request.form['name']
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO users (name) VALUES ('" + _name + "')");
+    data = cursor.fetchone()
+
+    if len(data) is 0:
+        return "Error"
+    else:
+        return "Success"
+
+
+ 
+if __name__ == "__main__":
+    app.run()
